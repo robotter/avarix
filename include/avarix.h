158d7ffdf9c20ea6ff8b84c97afb9b0da4e4bdbc
Benoît Ryder <benoit@ryder.fr>
2013-02-03 12:39:12 +0100

    Add a header file for common definitions
    The include/ directory is already handled by makefiles.

---
+++ include/avarix.h
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @brief Various common Avarix definitions
+ */
+#ifndef AVARIX_H__
+#define AVARIX_H__
+
+
+/// Interrupt level
+typedef enum {
+  INTLVL_NONE = 0,
+  INTLVL_LO = 1,
+  INTLVL_MED = 2,
+  INTLVL_HI = 3,
+
+} intlvl_t;
+
+
+#endif
+//@}
