72ba69fa339264c9209e16cfc6b55ecf70595312
Benoît Ryder <benoit@ryder.fr>
2013-12-18 00:39:37 +0100

    Fix py_templatize_rule when output name is provided with a leading space

--- mk/common.mk
+++ mk/common.mk
@@ -7,7 +7,7 @@
 # Define rules to generated a single template using PY_TEMPLATIZE
 #   py_templatize_rule <template> <output-name> [args] [deps]
 define py_templatize_rule
-$$(gen_dir)/$(2): $(1) $(4)
+$$(gen_dir)/$(strip $(2)): $(1) $(4)
 	@mkdir -p $$(dir $$@)
 	$(PY_TEMPLATIZE) -o $$@ -i $$< -- $(3)
 endef
