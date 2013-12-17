f223f4d3518c5db66cfd5fbf3d04065503d377ab
Benoît Ryder <benoit@ryder.fr>
2013-12-18 00:18:09 +0100

    Allow to specify extra dependencies in py_templatize_rule

--- mk/common.mk
+++ mk/common.mk
@@ -5,11 +5,11 @@
 
 
 # Define rules to generated a single template using PY_TEMPLATIZE
-#   py_templatize_rule <template> <output-name> [args]
+#   py_templatize_rule <template> <output-name> [args] [deps]
 define py_templatize_rule
-$$(gen_dir)/$(2): $(1)
+$$(gen_dir)/$(2): $(1) $(4)
 	@mkdir -p $$(dir $$@)
-	$(PY_TEMPLATIZE) -o $$@ -i $$^ -- $(3)
+	$(PY_TEMPLATIZE) -o $$@ -i $$< -- $(3)
 endef
 
 # Define rule to generate templates from a directory using PY_TEMPLATIZE
