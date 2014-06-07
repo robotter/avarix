58a831882fdc23da70524dc4bc449813537ad1ef
Benoît Ryder <benoit@ryder.fr>
2014-06-07 12:54:26 +0200

    Add header file with elements used for internal needs

---
+++ include/avarix/internal.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @brief Internal definitions for internal needs
+ */
+//@{
+#ifndef AVARIX_INTERNAL_H__
+#define AVARIX_INTERNAL_H__
+
+
+/** @name Preprocessor concatenation indirections */
+//@{
+
+#define AVARIX_CONCAT2(a,b)  a ## b
+#define AVARIX_EVALCONCAT2(a,b)  AVARIX_CONCAT2(a,b)
+
+#define AVARIX_CONCAT3(a,b,c)  a ## b ## c
+#define AVARIX_EVALCONCAT3(a,b,c)  AVARIX_CONCAT3(a,b,c)
+
+//@}
+
+
+#endif
+//@}
