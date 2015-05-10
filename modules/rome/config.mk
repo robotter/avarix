9aecb2ff3a9fe85bcc7970d1369e0c0f54607035
Benoît Ryder <benoit@ryder.fr>
2015-05-10 21:00:15 +0200

    rome: detect when rome_messages module changed
    When ROME_MESSAGES is not defined, the default rome_messages module is
    used. In such case, detect when its source file changed and trigger
    generation.
    
    It assumes that the file rome_messages module is a .py or .pyc file.
    Also, if the module is not found, an (explicit) Python traceback is
    output.

--- modules/rome/config.mk
+++ modules/rome/config.mk
@@ -3,9 +3,15 @@ MODULES = uart timer
 
 GEN_FILES = rome_msg.h
 
+ifeq ($(ROME_MESSAGES),)
+rome_msg_deps = $(shell python -c 'import rome_messages as m; print(m.__file__.replace(".pyc",".py"))')
+else
+rome_msg_deps = $(ROME_MESSAGES)
+endif
+
 $(eval $(call py_templatize_rule, \
 	$(src_dir)/rome_msg.tpl.h, rome_msg.h, \
 	$(src_dir)/rome_msg.py $(ROME_MESSAGES), \
-	$(src_dir)/rome_msg.py $(ROME_MESSAGES) \
+	$(src_dir)/rome_msg.py $(rome_msg_deps) \
 	))
 
