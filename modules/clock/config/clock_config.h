4f391366f7a6a9ff73aba8464178cf37526ebb50
Augustin <aboisvert@fly-n-sense.com>
2017-02-22 09:47:33 +0100

    clock: add run-time autocalibration option

--- modules/clock/config/clock_config.h
+++ modules/clock/config/clock_config.h
@@ -31,5 +31,12 @@
 #define CLOCK_PER2_FREQ  CLOCK_CPU_FREQ
 #define CLOCK_PER4_FREQ  CLOCK_CPU_FREQ
 
+/** @brief Enable automatic run-time calibration of internal clock
+ *
+ * Only usable with 2MHz or 32Mhz internal clocks.
+ * It will use the 32768kHz calibrated clock as the adjustement source.
+ */
+#undef CLOCK_AUTO_RUN_TIME_CALIBRATION
+
 ///@endcond
 //@}
