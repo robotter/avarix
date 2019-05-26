d7dd76cadab3bcb05235d0860cdfc22e381f74db
Benoît Ryder <benoit@ryder.fr>
2019-05-26 20:38:48 +0200

    Add an ARRAY_LEN macro

--- include/avarix.h
+++ include/avarix.h
@@ -12,7 +12,11 @@
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 /// Clamp a value to range [x;y]
 #define CLAMP(v,x,y) ((v) < (x) ? (x) : (v) > (y) ? (y) : (v))
-
+/// Return the length of an array (fails on pointer)
+#define ARRAY_LEN(a)  ({ \
+    _Static_assert(!__builtin_types_compatible_p(typeof(a), typeof(&a[0])), "expected array (not pointer)"); \
+    sizeof(a)/sizeof(a[0]); \
+  })
 
 #endif
 //@}
